527bbae9213d10dba5b87335bf2ef91336e5c8ba
emersonsvale <emersonsousa.vale@gmail.com>
2025-05-12 15:25:39 -0300

    Adicionada estrutura de API para Vercel

---
+++ api/index.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from app.main import app as main_app
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/docs")
+
+# Importando todas as rotas do app principal
+app.mount("/", main_app) 
